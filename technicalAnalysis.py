fcdd1e9818fed395489fadea06ea5293dd14159b
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2020-02-12 23:47:27 +0530

    Update technicalAnalysis.py

--- technicalAnalysis.py
+++ technicalAnalysis.py
@@ -7,7 +7,7 @@ from math import pow
 import datetime
 import matplotlib.pyplot as plot
 import matplotlib.dates
-from mpl_finance import candlestick_ohlc
+from mpl_finance import candlestick_ohlc 
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
